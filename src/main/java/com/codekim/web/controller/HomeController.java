ab64231a79f6aeb22818a0e86f4d528eaaecb7f6
codekim <anfud162@naver.com>
2022-09-21 20:00:36 +0900

    Commit Start

---
+++ src/main/java/com/codekim/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codekim.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+ 
+	@RequestMapping("/index")
+	public String abcd() {
+	   return "123";
+   }
+}
